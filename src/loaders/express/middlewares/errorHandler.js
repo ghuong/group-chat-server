6be38640eedfa8e10277061b921a374e9bab1b36
Gary Huong <garyhuong@gmail.com>
2021-03-15 06:56:00 -0400

    Modify errorHandler to return 500

--- src/loaders/express/middlewares/errorHandler.js
+++ src/loaders/express/middlewares/errorHandler.js
@@ -17,5 +17,9 @@ module.exports = (err, req, res, next) => {
       return res.status(400).json({ error: err.message }).end();
   }
 
-  return next(err);
+  if (res.headersSent) {
+    return next(err);
+  } else {
+    return res.status(err.status || 500).render("500");
+  }
 };
