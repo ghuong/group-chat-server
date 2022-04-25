11f48291dd40f78752547cd8ee6c125aef272976
Gary Huong <garyhuong@gmail.com>
2022-04-25 16:55:47 -0400

    Logging hello world upon start for debugging heroku

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const config = require("./config");
 const logger = require("@logger");
 
 (async () => {
+  logger.info("Hello world!");
+
   const httpServer = await require("./loaders")();
 
   httpServer.listen(config.port, () => {
