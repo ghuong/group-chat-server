0c0b5513b39bf747702deab49517c3098d782392
Gary Huong <garyhuong@gmail.com>
2022-04-26 08:48:33 -0400

    Remove Procfile; In production, don't check for .env file

--- src/config/index.js
+++ src/config/index.js
@@ -6,16 +6,18 @@ require("module-alias").addAliases({
   "@services": `${__dirname}/../services`,
 });
 
-const dotenv = require("dotenv");
-
-const envFound = dotenv.config();
-if (envFound.error) {
-  // This error should crash whole process
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
-
 // Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV ||= "development";
+
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = require("dotenv");
+
+  const envFound = dotenv.config();
+  if (envFound.error) {
+    // This error should crash whole process
+    throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  }
+}
 
 module.exports = {
   /**
