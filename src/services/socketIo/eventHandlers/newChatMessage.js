63ac497e58316d846fd53c38351d68cef233149f
Gary Huong <garyhuong@gmail.com>
2022-05-13 22:01:45 -0400

    Hotfix bug

--- src/services/socketIo/eventHandlers/newChatMessage.js
+++ src/services/socketIo/eventHandlers/newChatMessage.js
@@ -3,7 +3,7 @@ const logger = require("@logger");
 function handleNewChatMessage(socketService) {
   logger.info(
     `Received from: Room '${socketService.getRoomId()}',`,
-    `Socket '${socketService.getUserId().substring(0, 6)}..':`,
+    `Socket '${socketService.getUser().id.substring(0, 6)}..':`,
     `"${socketService.getMessage().substring(0, 30)}"`
   );
   socketService.sendToAllInRoom(); // broadcast message to everyone in same room
